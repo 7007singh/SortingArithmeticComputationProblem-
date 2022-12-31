24b9a74ca41a819a812bfdf2f115f01d5ee850bf
7007singh <shw12021982@gmail.com>
2022-12-31 15:50:46 +0530

    Added: First Arithmetic Computatio added

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -3,3 +3,6 @@ echo "Welcome to Sorting Arithmetic Computation Problem"
 read -p "enter first value : " a;
 read -p "enter second value : " b;
 read -p "enter third value : " c;
+
+w=$(($a+($b*$c)));
+echo "$a + $b*$c = $w";
